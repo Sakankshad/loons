d863ce0d13b5ab5d04aa4f9ec7546a0ee2d29183
Sakankshad <53963736+Sakankshad@users.noreply.github.com>
2020-11-03 19:47:13 +0530

    content writing page added

--- js/demos.js
+++ js/demos.js
@@ -41,6 +41,48 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  var typed1 = new Typed('#typed1', {
+    stringsElement: '#typed-strings1',
+    typeSpeed: 40,
+    backSpeed: 15,
+    startDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+    onBegin: function(self) {
+      prettyLog('onBegin ' + self);
+    },
+    onComplete: function(self) {
+      prettyLog('onComplete ' + self);
+    },
+    preStringTyped: function(pos, self) {
+      prettyLog('preStringTyped ' + pos + ' ' + self);
+    },
+    onStringTyped: function(pos, self) {
+      prettyLog('onStringTyped ' + pos + ' ' + self);
+    },
+    onLastStringBackspaced: function(self) {
+      prettyLog('onLastStringBackspaced ' + self);
+    },
+    onTypingPaused: function(pos, self) {
+      prettyLog('onTypingPaused ' + pos + ' ' + self);
+    },
+    onTypingResumed: function(pos, self) {
+      prettyLog('onTypingResumed ' + pos + ' ' + self);
+    },
+    onReset: function(self) {
+      prettyLog('onReset ' + self);
+    },
+    onStop: function(pos, self) {
+      prettyLog('onStop ' + pos + ' ' + self);
+    },
+    onStart: function(pos, self) {
+      prettyLog('onStart ' + pos + ' ' + self);
+    },
+    onDestroy: function(self) {
+      prettyLog('onDestroy ' + self);
+    }
+  });
+
   document.querySelector('.toggle').addEventListener('click', function() {
     typed.toggle();
   });
